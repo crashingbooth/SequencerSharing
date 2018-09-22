4b100193b29bf64d50c497dccd3906ce44c9d8fb
crashing booth <jholtzkener@hotmail.com>
2018-09-22 18:42:47 -0400

    firebase structs

--- SharingTracksWIthFireStore/Track.swift
+++ SharingTracksWIthFireStore/Track.swift
@@ -7,8 +7,26 @@
 //
 
 import Foundation
+import AudioKit
 
 struct Track {
     var trackNum: Int
+    var events: [MIDIEvent]
     
+}
+
+struct MIDIEvent {
+    var noteNumber: MIDINoteNumber
+    var velocity: MIDIVelocity
+    var channel: MIDIChannel
+    var position: Double
+    var duration: Double
+    
+    init(data: AKMIDINoteData){
+        self.noteNumber = data.noteNumber
+        self.velocity = data.velocity
+        self.channel = data.channel
+        self.position = data.position.beats
+        self.duration = data.position.beats
+    }
 }
