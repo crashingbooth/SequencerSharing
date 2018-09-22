8474a7da540fccf2f7ab5b3e550466685678cf07
crashing booth <jholtzkener@hotmail.com>
2018-09-22 08:25:12 -0400

    Initial Commit

---
+++ SharingTracksWIthFireStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SharingTracksWIthFireStore
+//
+//  Created by Jeff Holtzkener on 2018-09-22.
+//  Copyright © 2018 crashingbooth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
