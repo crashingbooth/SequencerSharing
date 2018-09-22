0aade129345821d86fcd5ddfa51d19b81ec8ea5a
crashing booth <jholtzkener@hotmail.com>
2018-09-22 18:36:43 -0400

    added track

---
+++ SharingTracksWIthFireStore/Track.swift
@@ -0,0 +1,14 @@
+//
+//  Track.swift
+//  SharingTracksWIthFireStore
+//
+//  Created by Jeff Holtzkener on 2018-09-22.
+//  Copyright © 2018 crashingbooth. All rights reserved.
+//
+
+import Foundation
+
+struct Track {
+    var trackNum: Int
+    
+}
