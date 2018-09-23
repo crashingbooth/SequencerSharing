f492c7ba5fdd5749b0679d79ada6d09a5122dd2b
crashing booth <jholtzkener@hotmail.com>
2018-09-23 08:06:34 -0400

    connected buttons

---
+++ SharingTracksWIthFireStore/SharedSequencerVC.swift
@@ -0,0 +1,34 @@
+//
+//  SharedSequencerVC.swift
+//  SharingTracksWIthFireStore
+//
+//  Created by Jeff Holtzkener on 2018-09-23.
+//  Copyright © 2018 crashingbooth. All rights reserved.
+//
+
+import UIKit
+
+class SharedSequencerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func loadFile(_ sender: Any) {
+    }
+    
+    @IBAction func sendTracks(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
