cf500bb122310870123908b9bebed67fb76ff38f
crashing booth <jholtzkener@hotmail.com>
2018-09-25 07:28:44 -0400

    re-added TrackCell

---
+++ SharingTracksWIthFireStore/TrackCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrackCell.swift
+//  SharingTracksWIthFireStore
+//
+//  Created by Jeff Holtzkener on 2018-09-25.
+//  Copyright © 2018 crashingbooth. All rights reserved.
+//
+
+import UIKit
+
+class TrackCell: UICollectionViewCell {
+    
+}
