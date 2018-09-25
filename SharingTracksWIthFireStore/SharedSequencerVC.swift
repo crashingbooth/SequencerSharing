458e81482347d8d51b315c0c594389a50209ada8
crashing booth <jholtzkener@hotmail.com>
2018-09-25 07:06:45 -0400

    added collectionView

--- SharingTracksWIthFireStore/SharedSequencerVC.swift
+++ SharingTracksWIthFireStore/SharedSequencerVC.swift
@@ -10,6 +10,8 @@ import UIKit
 import Firebase
 
 class SharedSequencerVC: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     var customSeq: CustomSequencer!
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ SharingTracksWIthFireStore/trackCell.swift
@@ -0,0 +1,13 @@
+//
+//  trackCell.swift
+//  SharingTracksWIthFireStore
+//
+//  Created by Jeff Holtzkener on 2018-09-25.
+//  Copyright © 2018 crashingbooth. All rights reserved.
+//
+
+import UIKit
+
+class trackCell: UICollectionViewCell {
+    
+}
