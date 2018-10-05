79c0d419a27a53c8213e0317cf6fb45d1875f3d8
crashing booth <jholtzkener@hotmail.com>
2018-10-05 08:48:46 -0400

    with blog stuff, to remove

---
+++ SharingTracksWIthFireStore/SequencerExample.swift
@@ -0,0 +1,12 @@
+//
+//  SequencerExample.swift
+//  SharingTracksWIthFireStore
+//
+//  Created by Jeff Holtzkener on 2018-10-02.
+//  Copyright © 2018 crashingbooth. All rights reserved.
+//
+
+import Foundation
+import AudioKit
+
+
